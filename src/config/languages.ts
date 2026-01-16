cbaadbdf0e250e784e864d3f3656fef2350f2d68
Kaleb <kalebzambrano@outlook.com>
2026-01-16 18:44:11 -0500

    changing config and languages

---
+++ src/config/languages.ts
@@ -0,0 +1,23 @@
+export const LANGUAGES = {
+  es: {
+    label: "ES",
+    flag: "es",
+    path: "/",
+    default: true,
+  },
+  en: {
+    label: "EN",
+    flag: "us",
+    path: "/en",
+  },
+  fr: {
+    label: "FR",
+    flag: "fr",
+    path: "/fr",
+  },
+  pt: {
+    label: "PT",
+    flag: "pt",
+    path: "/pt",
+  },
+} as const
