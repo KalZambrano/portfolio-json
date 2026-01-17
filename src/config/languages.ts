4b226bbd12f5d0b1243a30be41919f06559cae06
Kaleb <kalebzambrano@outlook.com>
2026-01-16 20:00:33 -0500

    adding dynamic path

--- src/config/languages.ts
+++ src/config/languages.ts
@@ -4,20 +4,24 @@ export const LANGUAGES = {
     flag: "es",
     path: "/",
     default: true,
+    json: "cv_spanish.json",
   },
   en: {
     label: "EN",
     flag: "us",
     path: "/en",
+    json: "cv_english.json",
   },
   fr: {
     label: "FR",
     flag: "fr",
     path: "/fr",
+    json: "cv_french.json",
   },
   pt: {
     label: "PT",
     flag: "pt",
     path: "/pt",
+    json: "cv_portuguese.json",
   },
 } as const
